6f6ba717035e0237992a4c588defbee3c70b268d
Ari Gilchrist <u7428345@anu.edu.au>
2025-08-24 21:58:58 +1000

    Minor fixes

--- compiler/parse_pqasm.py
+++ compiler/parse_pqasm.py
@@ -1,18 +1,18 @@
 # By chatGPT
 
 from antlr4 import *
-from PQASMCodeLexer import PQASMCodeLexer
-from PQASMCodeParser import PQASMCodeParser
+from PQASMLexer import PQASMLexer
+from PQASMParser import PQASMParser
 
 # Load the PQASM filepython parse_pqasm.py
 input_stream = FileStream("test.pqasm")
 
 # Create a lexer and token stream
-lexer = PQASMCodeLexer(input_stream)
+lexer = PQASMLexer(input_stream)
 token_stream = CommonTokenStream(lexer)
 
 # Create a parser and parse the file starting from the 'program' rule
-parser = PQASMCodeParser(token_stream)
+parser = PQASMParser(token_stream)
 tree = parser.program()  # 'program' is your start rule
 
 # Optional: print the parse tree as text
