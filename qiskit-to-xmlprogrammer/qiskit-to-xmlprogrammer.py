0e83586fc1b344fa7f2ea8e14d72f7d9ffe7ae0d
Ari Gilchrist <u7428345@anu.edu.au>
2025-09-06 21:19:14 +1000

    Initial functioning visitor

--- qiskit-to-xmlprogrammer/qiskit-to-xmlprogrammer.py
+++ qiskit-to-xmlprogrammer/qiskit-to-xmlprogrammer.py
@@ -1,7 +1,18 @@
+"""
+
+This file contains a visitor which traverses a Qiskit DAG (Directed Acyclic
+Graph - a representation of a quantum circuit) and converts it into the format 
+required for "XMLProgrammer.py".
+
+"""
+
+
 import qiskit
 from qiskit import QuantumCircuit
 from qiskit.converters import circuit_to_dag
 from qiskit.dagcircuit import DAGInNode, DAGOpNode, DAGNode, DAGOutNode
+
+from XMLProgrammer import QXProgram, QXQID, QXCU, QXX, QXTop, QXExp, QXH
 
 
 # Example circuit
@@ -14,21 +25,48 @@ print(qc.draw())
 # Convert to DAG
 dagEx = circuit_to_dag(qc)
 
-# Create a visitor (class which goes thorugh the DAG)
+
+# ------------------------- DAG TO XMLPROGRAMMER -------------------------------
+
+
 class DAGVisitor:
-    def __init__(self, dag, func):
+    def __init__(self, dag, func = "nodeToXMLProgrammer"):
         self.dag = dag
-        self.func = func
-        self.visitedNodes = set()
-        # Not recording non-visited nodes won't matter, since if they can't be reached
-        #, they have no effect on the code anyways
+        
+        if func == "printDAG":
+            self.func = self.printNodeAndType
+        elif func == "nodeToXMLProgrammer":
+            self.func = self.nodeToXMLProgrammer
+        else:
+            raise ValueError("Unknown function for visitor for visitor")
+
+        # Dictionary mapping Qiskit qubits to XMLProgrammer qubits
+        self.XMLQubits = dict()
+        for qubit in dag.qubits:
+            self.XMLQubits[qubit] = QXQID(str(qubit._index))
+
+
+    def printNodeAndType(self, node):
+        if isinstance(node, DAGInNode):
+            print("Input Node for wire: ", node.wire)
+        elif isinstance(node, DAGOutNode):
+            print("Output Node for wire: ", node.wire)
+        elif isinstance(node, DAGOpNode):
+            print("Operation Node: ", node.op.base_class, " named ", node.name)
+        else:
+            print("???")
+
 
     def startVisit(self):
         self.visitedNodes = set()
-        startNodes = set()
-
-        for startingNode in dagEx.input_map.values():
+        self.expList = []
+        
+        for startingNode in self.dag.input_map.values():
             self.visitNode(startingNode)
+
+        self.program = QXProgram(self.expList)
+        print("Extracted program in XMLProgrammer format:")
+        print(self.program)
 
         
     def visitNode(self, node):
@@ -39,18 +77,46 @@ class DAGVisitor:
             self.func(node)
             for successor in self.dag.successors(node):
                 self.visitNode(successor)
+
+
+    def nodeToXMLProgrammer(self, node):
+        if isinstance(node, DAGInNode):
+            pass
+        elif isinstance(node, DAGOutNode):
+            pass
+        elif isinstance(node, DAGOpNode):
+            inputBits = [self.XMLQubits[q] for q in node.qargs]
+
+            # Map the operation to XMLProgrammer format
+            exp = None
+            # Controlled not:
+            if node.name == "cx":
+                exp = QXCU("cx", inputBits[0], QXProgram([QXX("x", inputBits[1])]))
+            elif node.name == "h":
+                exp = QXH("h", inputBits[0])
+            else:
+                pass
+
+            print("Expression found:", exp)
+
+            # Turn the extracted operation into an expression, and add it to
+            # the list of expressions
+            if exp is not None:
+                self.expList.append(exp)
             
+            
+# ------------------------------- EXAMPLE USAGE --------------------------------
 
-# example usage: just print everything out:
-def printNodeAndType(node):
-    if isinstance(node, DAGInNode):
-        print("Input Node for wire: ", node.wire)
-    elif isinstance(node, DAGOutNode):
-        print("Output Node for wire: ", node.wire)
-    elif isinstance(node, DAGOpNode):
-        print("Operation Node: ", node.op.base_class, " named ", node.name)
-    else:
-        print("???")
+# DAG Printer
+#printerVistor = DAGVisitor(dagEx, "printDAG")
+#printerVistor.startVisit()
 
-printerVistor = DAGVisitor(dagEx, printNodeAndType)
-printerVistor.startVisit()
+
+# DAG to XMLProgrammer
+xmlProgrammerVisitor = DAGVisitor(dagEx)
+xmlProgrammerVisitor.startVisit()
+
+
+
+
+
